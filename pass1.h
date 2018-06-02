6b6f38e68be0ea8f6a081c2c4bccf4208e4af1e9
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2018-06-02 12:07:44 +0200

    Updated header files.

--- pass1.h
+++ pass1.h
@@ -1,4 +1,4 @@
 
 char *token_reader_pass1( char *start, char *ptr, unsigned short lastToken, unsigned short token, int tokenlength );
 void pass1_reader( char *start, int tokenlength );
-
+int findLabelRef( char *name );
