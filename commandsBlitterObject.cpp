642fb42e6ff58272dd76ba1ecd53e6584759a212
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2018-06-26 21:56:20 +0200

    Prototype implementation of bob command (needs a lot more work.)

--- commandsBlitterObject.cpp
+++ commandsBlitterObject.cpp
@@ -28,12 +28,36 @@ extern int current_screen;
 extern struct retroScreen *screens[8] ;
 extern struct retroVideo *video;
 extern struct retroRGB DefaultPalette[256];
-
+extern struct retroSprite *sprite;
+extern struct retroSpriteObject bobs[30];
 
 char *_boBob( struct glueCommands *data, int nextToken )
 {
 	int args = stack - data->stack +1 ;
+	int num,image;
 	proc_names_printf("%s:%d\n",__FUNCTION__,__LINE__);
+
+	printf("args: %d\n",args);
+
+	num = getStackNum( stack - 3 );
+	stack_get_if_int( stack - 2 , &bobs[num].x );
+	stack_get_if_int( stack - 1 , &bobs[num].y );
+	image = getStackNum( stack );
+
+	if (sprite)
+	{
+		image = image % sprite -> number_of_frames;
+
+		bobs[num].sprite = sprite ;
+		bobs[num].frame = &sprite -> frames[image];
+
+		if (screens[current_screen])
+		{
+			retroPasteSprite(screens[current_screen], sprite, bobs[num].x, bobs[num].y, image);
+		}
+	}
+
+	printf( "bob %d,%d,%d,%d\n", num,bobs[num].x,bobs[num].y,image );
 
 	popStack( stack - data->stack );
 	return NULL;
