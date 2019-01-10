5feb8b5fd3bd5b81a8dbea9757e1671b0a9b93b8
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2019-01-10 22:09:19 +0100

    Amal test app, does have real screens.

--- screen_helper.cpp
+++ screen_helper.cpp
@@ -1,11 +1,18 @@
+
+#include "stdafx.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#if defined(__amigaos4__) || defined(__amigaos__)
 #include <proto/exec.h>
 #include <proto/dos.h>
 #include <proto/retroMode.h>
+#endif
 #include "commandsScreens.h"
 
 extern struct retroScreen *screens[8] ;
+
+#ifndef test_app
 
 int XScreen_formula( struct retroScreen *screen, int x )
 {
@@ -35,3 +42,33 @@ int YHard_formula( struct retroScreen *screen, int y )
 	return y;
 }
 
+#else
+
+struct retroScreen
+{
+	void *ptr;
+};
+
+
+int XScreen_formula(struct retroScreen *screen, int x)
+{
+	return x;
+}
+
+int YScreen_formula(struct retroScreen *screen, int y)
+{
+	return y;
+}
+
+int XHard_formula(struct retroScreen *screen, int x)
+{
+	return x;
+}
+
+int YHard_formula(struct retroScreen *screen, int y)
+{
+	return y;
+}
+
+
+#endif
