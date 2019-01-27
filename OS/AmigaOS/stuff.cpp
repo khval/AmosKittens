1870cd4289b4f58e74dc17bdc07c507dc35482a2
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2019-01-27 22:42:53 +0100

    A replacement for none standard Printf

---
+++ OS/AmigaOS/stuff.cpp
@@ -0,0 +1,16 @@
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+void Printf_iso(const char *fmt,...)
+{
+	char buffer[2000];
+	va_list args;
+	va_start (args, fmt);
+	vsprintf(buffer,fmt,args);
+	va_end(args);
+
+	Printf("%s",buffer);
+}
