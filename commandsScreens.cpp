c6b4ea58c0b324ca14b5408988d7b862580cbb85
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2019-04-27 23:52:04 +0200

    Fixed: Screen Display x = 128 should be min x position for screens.

--- commandsScreens.cpp
+++ commandsScreens.cpp
@@ -253,7 +253,7 @@ char *_gfxScreenDisplay( struct glueCommands *data, int nextToken )
 
 			if (screen = screens[screen_num])
 			{
-				if (kittyStack[stack-3].type ==  type_int) screen -> scanline_x = getStackNum( stack-3 );
+				if (kittyStack[stack-3].type ==  type_int) screen -> scanline_x = getStackNum( stack-3 ) - 128;
 				if (kittyStack[stack-2].type ==  type_int) screen -> scanline_y = (getStackNum( stack-2 ) *2) - 80;
 				if (kittyStack[stack-1].type ==  type_int) screen -> displayWidth = getStackNum( stack-1 );
 				if (kittyStack[stack].type ==  type_int) screen -> displayHeight = getStackNum( stack );
