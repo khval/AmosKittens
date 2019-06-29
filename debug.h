e012618d36fe580f5d107288c6894db6ba7b5eec
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2019-06-29 21:24:28 +0200

    works better when fast execute is off.

--- debug.h
+++ debug.h
@@ -30,7 +30,7 @@
 // ------------ optimizer ----------------------
 
 #define run_program_yes
-#define enable_fast_execution_yes		// Some debug option do not work when this enabled.
+#define enable_fast_execution_no		// Some debug option do not work when this is enabled.
 
 //------------- end of options -----------------
 
