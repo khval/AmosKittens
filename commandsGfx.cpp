b9f4de7881ed26bebcf1b4d75bd33dccc62adddc
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2019-07-20 23:05:27 +0200

    "Wait Vbl" now triggers bobUpdate

--- commandsGfx.cpp
+++ commandsGfx.cpp
@@ -144,12 +144,15 @@ char *_gfxColour( struct glueCommands *data, int nextToken )
 	return NULL;
 }
 
+extern int bobUpdate;
+
 char *gfxWaitVbl(struct nativeCommand *cmd, char *tokenBuffer)
 {
 #if defined(__amigaos4__) || defined(__morphos__) || defined(__aros__)
 	if (( sig_main_vbl )&&( EngineTask ))
 	{
 		Wait(1<<sig_main_vbl);
+		bobUpdate = 1;
 	}
 	else
 	{
