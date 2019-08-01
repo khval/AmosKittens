5307b182b717096b081822f7fcf063ee9848fbb5
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2019-08-01 18:07:39 +0200

    Missing Screen Width token added.

--- AmosKittens.cpp
+++ AmosKittens.cpp
@@ -999,6 +999,7 @@ struct nativeCommand nativeCommands[]=
 	{0x0B34,"Auto View On", 0, ocAutoViewOn },
 	{0x0B46,"Screen Base", 0, gfxScreenBase },
 	{0x0B58,"Screen Width", 0, gfxScreenWidth },
+	{0x0B6C,"Screen Width", 0, gfxScreenWidth },			// =Screen Width( screen nr)
 	{0x0B88,"Screen Height", 0, gfxScreenHeight },		// =Screen Height(screen nr)
 	{0x0B74,"Screen Height", 0, gfxScreenHeight },		// =Screen Height
 	{0x0B90,"Get Palette",0,gfxGetPalette },
