806504a881af506e8040a0a49ae546253d72ce0a
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2019-11-03 19:04:25 +0100

    Support for "screen size" object.

--- amal_object.h
+++ amal_object.h
@@ -4,4 +4,5 @@ extern struct channelAPI sprite_api;
 extern struct channelAPI rainbow_api;
 extern struct channelAPI screen_display_api;
 extern struct channelAPI screen_offset_api;
+extern struct channelAPI screen_size_api;
 
---
+++ amal_object_screen_size.cpp
@@ -0,0 +1,89 @@
+#include "stdafx.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+
+#ifdef __amigaos4__
+#include <proto/dos.h>
+#include <libraries/retroMode.h>
+#include <proto/retroMode.h>
+#endif
+
+#ifdef __linux__
+#include <stdint.h>
+#include "os/linux/stuff.h"
+#include <retromode.h>
+#include <retromode_lib.h>
+#define Pritnf printf
+#endif
+
+#include "AmalCompiler.h"
+#include "channel.h"
+
+extern struct retroVideo *video;
+extern struct retroScreen *screens[8] ;
+
+static int getMax ( void )
+{
+	return 8;
+}
+
+static int getImage (int object)
+{
+	return 0;
+}
+
+static int getX (int object)
+{
+	return screens[object]->displayWidth;
+}
+
+static int getY (int object)
+{
+	return screens[object]->displayHeight;
+}
+
+static void setImage (int object,int image)
+{
+}
+
+static void setX (int object,int x)
+{
+	screens[object]->displayWidth = x;
+
+	retroApplyScreen( screens[object], video, 
+			screens[object] -> scanline_x,
+			screens[object] -> scanline_y,
+			screens[object] -> displayWidth,
+			screens[object] -> displayHeight );
+
+	video -> refreshAllScanlines = TRUE;
+}
+
+static void setY (int object,int y)
+{
+	screens[object]->displayHeight = y;
+
+	retroApplyScreen( screens[object], video, 
+			screens[object] -> scanline_x,
+			screens[object] -> scanline_y,
+			screens[object] -> displayWidth,
+			screens[object] -> displayHeight );
+
+	video -> refreshAllScanlines = TRUE;
+}
+
+
+struct channelAPI screen_size_api = 
+{
+	getMax,
+	getImage,
+	getX,
+	getY,
+	setImage,
+	setX,
+	setY
+};
+
