f0158f4d331dd9c921168417f78257a1e11d6052
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2019-12-16 22:36:12 +0100

    Added a few comments.

--- amosString.h
+++ amosString.h
@@ -6,6 +6,6 @@ extern struct stringData *amos_mid( struct stringData *var, int start, int size 
 extern struct stringData *amos_right( struct stringData *var, int size );
 extern int amos_instr( struct stringData *var,int start,struct stringData *find  );
 extern struct stringData *toAmosString( const char *txt,int len);
-extern struct stringData *toAmosString_char(char *adr, char t);
-extern struct stringData *toAmosString_len_or_char(char *adr, int len, char t);
+extern struct stringData *toAmosString_char(char *adr, char t);				// Warning will count chars internally, use toAmosString(sdr,len) if you have a count.
+extern struct stringData *toAmosString_len_or_char(char *adr, int len, char t);	// this function is for cropping,
 
