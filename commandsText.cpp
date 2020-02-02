d829f4e81488f1c771c283f3f8e14b5e048fe511
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2020-02-02 13:27:40 +0100

    Print command changed, fix for tab space bug, change to line freed system.

--- commandsText.cpp
+++ commandsText.cpp
@@ -387,14 +387,26 @@ char *_print( struct glueCommands *data, int nextToken )
 	struct retroScreen *screen = screens[current_screen];
 	int n;
 
+	next_print_line_feed = true;
+
 	flushCmdParaStack( nextToken );
 	if (screen)
 	{
+		int type;
 		struct retroTextWindow *textWindow = screen -> currentTextWindow;
 
 		for (n=data->stack;n<=stack;n++)
 		{
-			switch (kittyStack[n].type)
+			type = kittyStack[n].type;
+
+			if (n>data->stack)
+			{
+				if (type != type_none)
+				{
+					if ( n < stack ) textWindow -> locateX += textWindow -> locateX % _tab_size ? _tab_size - textWindow -> locateX % _tab_size : 0;
+				}
+			}
+			switch (type)
 			{
 				case type_int:
 					__print_num( screen, kittyStack[n].integer.value);
@@ -409,8 +421,12 @@ char *_print( struct glueCommands *data, int nextToken )
 					if (n>data->stack) next_print_line_feed = false;
 					break;
 			}
+		}
 
-			if ( n < stack ) textWindow -> locateX += textWindow -> locateX % _tab_size ? _tab_size - textWindow -> locateX % _tab_size : 0;
+		if (next_print_line_feed)	// this is a hack, need to scroll screen if locateY is outside of window.
+		{
+			textWindow -> locateX = 0;
+			textWindow -> locateY += 1;
 		}
 
 		draw_cursor(screen);
