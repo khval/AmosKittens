ebb126ff9a5edb3510d43e9ffe37af515bad623a
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2022-08-25 17:52:08 +0200

    Probably a mistake

--- amal_object_sprite.h
+++ amal_object_sprite.h
@@ -1,9 +1,9 @@
 
-extern int XSprite_formula(int x);
-extern int YSprite_formula(int y);
+//extern int XSprite_formula(int x);
+//extern int YSprite_formula(int y);
 
 // convert from RetroMode cordinate, into AMOS sprite coordinate.
 
-extern int from_XSprite_formula(int x);
-extern int from_YSprite_formula(int y);
+//extern int from_XSprite_formula(int x);
+//extern int from_YSprite_formula(int y);
 
